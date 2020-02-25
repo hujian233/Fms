1f913a0b4f68d8df53c20e8ff9f54b9f838cf42a
Banana-Boat <53339979+Banana-Boat@users.noreply.github.com>
2020-02-25 20:25:41 +0800

    登录页面响应式/重置密码方式修改

--- JS/Login.js
+++ JS/Login.js
@@ -1,101 +1,127 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//客户端校验
-var id_is_legal = false;
-var pwd_is_legal = false;
-var id_reg = new RegExp('^[0-9]{7}$');
-var password_reg = new RegExp('^[a-zA-Z0-9]{6,12}$');
+//登录窗校验
+{
+    var id_is_legal = false;
+    var pwd_is_legal = false;
+    var id_reg = new RegExp('^[0-9]{7}$');
+    var password_reg = new RegExp('^[a-zA-Z0-9]{6,12}$');
 
-$('#userID').change(function(){
-    if(!id_reg.test($('#userID').val())){
-        $('#tip').text("请输入正确格式的工号");
-        $('#tip').css('display', 'block');
-    }else{
-        id_is_legal = true;
-        $('#tip').css('display', 'none');
-    }
-});
-$('#password').change(function(){
-    if(!password_reg.test($('#password').val())){
-        $('#tip').text("请输入正确格式的密码");
-        $('#tip').css('display', 'block');
-    }else{
-        pwd_is_legal = true;
-        $('#tip').css('display', 'none');
-    }
-});
-function validate(){
-    if(id_is_legal == true && pwd_is_legal == true){
-        return true;
-    }else{
-        return false;
+    $('#userID').change(function(){
+        if(!id_reg.test($('#userID').val())){
+            id_is_legal = true;
+            $('#tip').text("请输入正确格式的工号");
+            $('#tip').css('display', 'block');
+        }else{
+            id_is_legal = true;
+            $('#tip').css('display', 'none');
+        }
+    });
+    $('#password').change(function(){
+        if(!password_reg.test($('#password').val())){
+            pwd_is_legal = true;
+            $('#tip').text("请输入正确格式的密码");
+            $('#tip').css('display', 'block');
+        }else{
+            pwd_is_legal = true;
+            $('#tip').css('display', 'none');
+        }
+    });
+    function validate(){
+        if(id_is_legal == true && pwd_is_legal == true){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//刷卡重置密码
-$('#reset_password').click(function(){
-    if(id_is_legal){
-        alert('请刷卡，系统将验证信息...');
-        try{
-            //var cartNo = readCard();   //readCard()函数调用读卡器接口，返回员工卡号
-            $.ajax({
-                type : 'post',
-                dataType : 'json',
-                url : 'http://服务器名/mesvc01/api/emploee/query/' + cartNo.toString(),
-                error : alert('获取信息失败，请稍后重试...'),
-                success : function(data){
-                    if(data[3] == $('#userID').val()){
-                        $('#resetModal').modal('show');
-                    } else{
-                        alert('验证失败，工号与磁卡信息不匹配...');
+//申请重置密码（模态窗）
+{
+    $('#reset_password').click(function(){
+        $('#resetModal').modal('show');
+    });
+    
+    var _id_is_legal = false;
+    var email_is_legal = false;
+    var valiNum_is_legal = false;
+    var email_reg = new RegExp('^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$');
+    var valiNum_reg = new RegExp('^[0-9]{4}$');
+    
+    $('#_userID').change(function(){
+        if(!id_reg.test($(this).val())){
+            _id_is_legal = true;
+            $('#_tip').text("请输入正确格式的工号");
+            $('#_tip').css('display', 'block');
+        }else{
+            _id_is_legal = true;
+            $('#_tip').css('display', 'none');
+        }
+    });
+    
+    $('#email').change(function(){
+        if(!email_reg.test($(this).val())){
+            email_is_legal = false;
+            $('#_tip').text("请输入正确格式的邮箱");
+            $('#_tip').css('display', 'block');
+        }else{
+            email_is_legal = true;
+            $('#_tip').css('display', 'none');
+        }
+    });
+    
+    //点击获取验证码按钮的事件
+    $('#getNum').click(function(){
+        if(_id_is_legal && email_is_legal){
+            var json_data = {
+                'userID': $('#_userID').val(),
+                'email': $('#email').val()  
+            };
+            $.ajax({              //向后端发送工号与该工号的绑定邮箱（json）
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(json_data),
+                url: ''           //后端action
+            });
+        }
+    });
+    
+    $('#valiNum').change(function(){
+        if(!valiNum_reg.test($(this).val())){
+            valiNum_is_legal = false;
+            $('#_tip').text("请输入正确格式的验证码");
+            $('#_tip').css('display', 'block');
+        }else{
+            valiNum_is_legal = true;
+            $('#_tip').css('display', 'none');
+        }
+    });
+    
+    //点击提交申请按钮事件
+    $('#valiBtn').click(function(){
+        if(_id_is_legal && email_is_legal && valiNum_is_legal){
+            var json_data = {
+                'userID': $('#_userID').val(),
+                'email': $('#email').val(),
+                'valiNum': $('#valiNum').val()
+            };
+            $.ajax({              
+                type: 'POST',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(json_data),
+                url: '',           //后端action
+                success:function(data){
+                    var result = JSON.parse(data);
+                    if(result['status'] == 'success'){
+                        alert('已成功向管理员提交重置密码申请，请耐心等待通知！');
+                    }else{
+                        alert('申请提交失败，请自行告知管理员...');
                     }
                 }
             });
-        }catch{
-            alert('获取信息失败，请稍后重试...');
-        }        
-    }else{
-        $('#tip').text("请输入正确格式的工号");
-        $('#tip').css('display', 'block');
-    }
-});
-
-//表单验证
-var is_password_legel = false;
-var is_rePassword_legal = false;
-var password_reg = new RegExp('^[a-zA-Z0-9]{6,12}$');
-
-$('#newPassword').change(function(){
-    if(!password_reg.test($(this).val())){
-        is_password_legel = false;
-        $(this).parent().parent().attr('class', 'form-group has-error has-feedback');
-        $(this).parent().children('span').remove();
-        $(this).parent().append('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
-    }else{
-        is_password_legel = true;
-        $(this).parent().parent().attr('class', 'form-group has-success has-feedback');
-        $(this).parent().children('span').remove();
-        $(this).parent().append('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
-    }
-});
-
-$('#rePassword').change(function(){
-    if($(this).val() != $('#newPassword').val()){
-        is_rePassword_legel = false;
-        $(this).parent().parent().attr('class', 'form-group has-error has-feedback');
-        $(this).parent().children('span').remove();
-        $(this).parent().append('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
-    }else{
-        is_rePassword_legel = true;
-        $(this).parent().parent().attr('class', 'form-group has-success has-feedback');
-        $(this).parent().children('span').remove();
-        $(this).parent().append('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
-    }
-});
-
-function validate(){
-    if(is_password_legel && is_rePassword_legal){
-        return true;
-    }else{
-        return false;
-    }
+        }
+    });
+        
 }
+
+
